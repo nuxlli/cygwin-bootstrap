be91e84f9e4ec6a7b5b44d03c966b39850f0ef31
Everton Ribeiro <nuxlli@gmail.com>
2012-08-05 23:36:05 -0300

    Fixing setup execution

--- cygwinInstall.ps1
+++ cygwinInstall.ps1
@@ -3,9 +3,16 @@ $mirror      = "http://box-soft.com/"
 $download    = Join-Path $home "Downloads\Cygwin"
 $setup_path  = Join-Path $download "setup.exe"
 $target_path = "C:\cygwin"
+$packages    = "curl openssh wget"
 
 # Download setup
-(New-Object System.Net.WebClient).DownloadFile("http://cygwin.com/setup.exe", $setup_path)
+if (-not (Test-Path $setup_path)) {
+  if (-not (Test-Path $download)) {
+    mkdir $download
+  }
+  (New-Object System.Net.WebClient).DownloadFile("http://cygwin.com/setup.exe", $setup_path)
+}
 
 # Run setup
-$setup_path -s $mirror -L $download -l $download -R $target -q -P curl -P vim -P openssh -P wget
+$packs = [string]::join(" -P ", $packages.split(" "))
+"$setup_path -s $mirror -L $download -l $download -R $target_path -q -P $packs" | iex
