177936ec779bcd72062ada2013380e919be2ea8e
Everton Ribeiro <nuxlli@gmail.com>
2012-08-10 16:10:32 -0300

    Initial implement to retrieve the base url

--- cygwinInstall.ps1
+++ cygwinInstall.ps1
@@ -1,9 +1,17 @@
+cls
+$args
+$url = [regex]::Replace($myInvocation.MyCommand.Definition, ".*(http://.*)'\).*", '$1')
+[System.Net.HttpWebRequest]::Create($url).GetResponse().ResponseUri.AbsoluteUri
+
+exit
+
 # Paths
-$mirror      = "http://box-soft.com/"
+$mirror      = "http://mirror.cs.vt.edu/pub/cygwin/cygwin"
 $download    = Join-Path $home "Downloads\Cygwin"
 $setup_path  = Join-Path $download "setup.exe"
 $target_path = "C:\cygwin"
-$packages    = "cygwin coreutils curl openssh wget"
+$packages    = "curl openssh wget"
+
 
 # Download setup
 if (-not (Test-Path $setup_path)) {
@@ -14,5 +22,11 @@ if (-not (Test-Path $setup_path)) {
 }
 
 # Run setup
-$packs = [string]::join(" -P ", $packages.split(" "))
-"$setup_path -A -N -s $mirror -l $download -R $target_path -q -P $packs" | iex
+$setup_cmd = "$setup_path --no-startmenu --quiet-mode --site $mirror --local-package-dir $download --root $target_path"
+$packs     = [string]::join(" -P ", $packages.split(" "))
+
+"$setup_cmd -P cygwin -P coreutils | out-null" # | iex
+"$setup_cmd -P $packs | out-null" # | iex
+
+# Run ssh install
+# "$(Join-Path "$(Join-Path $target_path "bin")" "bash.exe") 'date'" | iex
